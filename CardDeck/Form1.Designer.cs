6c6511f56a45a768748fc8622b89401ee2fe8267
willdavi260 <willdavi260@SDS-L-F71HC63>
2022-03-01 08:42:31 -0500

    Add project files.

---
+++ CardDeck/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace CardDeck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.shuffleButton = new System.Windows.Forms.Button();
+            this.dealButton = new System.Windows.Forms.Button();
+            this.dealerCardsLabel = new System.Windows.Forms.Label();
+            this.playerCardsLabel = new System.Windows.Forms.Label();
+            this.collectButton = new System.Windows.Forms.Button();
+            this.dealerLabel = new System.Windows.Forms.Label();
+            this.playerLabel = new System.Windows.Forms.Label();
+            this.availableBox = new System.Windows.Forms.GroupBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.availableBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // shuffleButton
+            // 
+            this.shuffleButton.BackColor = System.Drawing.Color.GreenYellow;
+            this.shuffleButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.shuffleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.shuffleButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shuffleButton.ForeColor = System.Drawing.Color.Black;
+            this.shuffleButton.Location = new System.Drawing.Point(24, 12);
+            this.shuffleButton.Name = "shuffleButton";
+            this.shuffleButton.Size = new System.Drawing.Size(89, 47);
+            this.shuffleButton.TabIndex = 0;
+            this.shuffleButton.Text = "Shuffle";
+            this.shuffleButton.UseVisualStyleBackColor = false;
+            this.shuffleButton.Click += new System.EventHandler(this.shuffleButton_Click);
+            // 
+            // dealButton
+            // 
+            this.dealButton.BackColor = System.Drawing.Color.GreenYellow;
+            this.dealButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.dealButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dealButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dealButton.ForeColor = System.Drawing.Color.Black;
+            this.dealButton.Location = new System.Drawing.Point(24, 65);
+            this.dealButton.Name = "dealButton";
+            this.dealButton.Size = new System.Drawing.Size(89, 47);
+            this.dealButton.TabIndex = 1;
+            this.dealButton.Text = "Deal";
+            this.dealButton.UseVisualStyleBackColor = false;
+            this.dealButton.Click += new System.EventHandler(this.dealButton_Click);
+            // 
+            // dealerCardsLabel
+            // 
+            this.dealerCardsLabel.AutoSize = true;
+            this.dealerCardsLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dealerCardsLabel.ForeColor = System.Drawing.Color.White;
+            this.dealerCardsLabel.Location = new System.Drawing.Point(260, 52);
+            this.dealerCardsLabel.Name = "dealerCardsLabel";
+            this.dealerCardsLabel.Size = new System.Drawing.Size(18, 18);
+            this.dealerCardsLabel.TabIndex = 2;
+            this.dealerCardsLabel.Text = "--";
+            this.dealerCardsLabel.Click += new System.EventHandler(this.dealerCardsLabel_Click);
+            // 
+            // playerCardsLabel
+            // 
+            this.playerCardsLabel.AutoSize = true;
+            this.playerCardsLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerCardsLabel.ForeColor = System.Drawing.Color.White;
+            this.playerCardsLabel.Location = new System.Drawing.Point(260, 102);
+            this.playerCardsLabel.Name = "playerCardsLabel";
+            this.playerCardsLabel.Size = new System.Drawing.Size(18, 18);
+            this.playerCardsLabel.TabIndex = 3;
+            this.playerCardsLabel.Text = "--";
+            // 
+            // collectButton
+            // 
+            this.collectButton.BackColor = System.Drawing.Color.GreenYellow;
+            this.collectButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.collectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.collectButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.collectButton.ForeColor = System.Drawing.Color.Black;
+            this.collectButton.Location = new System.Drawing.Point(24, 118);
+            this.collectButton.Name = "collectButton";
+            this.collectButton.Size = new System.Drawing.Size(89, 47);
+            this.collectButton.TabIndex = 4;
+            this.collectButton.Text = "Collect";
+            this.collectButton.UseVisualStyleBackColor = false;
+            this.collectButton.Click += new System.EventHandler(this.collectButton_Click);
+            // 
+            // dealerLabel
+            // 
+            this.dealerLabel.AutoSize = true;
+            this.dealerLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dealerLabel.ForeColor = System.Drawing.Color.White;
+            this.dealerLabel.Location = new System.Drawing.Point(140, 52);
+            this.dealerLabel.Name = "dealerLabel";
+            this.dealerLabel.Size = new System.Drawing.Size(114, 19);
+            this.dealerLabel.TabIndex = 9;
+            this.dealerLabel.Text = "Dealer Cards:";
+            // 
+            // playerLabel
+            // 
+            this.playerLabel.AutoSize = true;
+            this.playerLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerLabel.ForeColor = System.Drawing.Color.White;
+            this.playerLabel.Location = new System.Drawing.Point(140, 102);
+            this.playerLabel.Name = "playerLabel";
+            this.playerLabel.Size = new System.Drawing.Size(113, 19);
+            this.playerLabel.TabIndex = 10;
+            this.playerLabel.Text = "Player Cards:";
+            // 
+            // availableBox
+            // 
+            this.availableBox.Controls.Add(this.outputLabel);
+            this.availableBox.ForeColor = System.Drawing.Color.White;
+            this.availableBox.Location = new System.Drawing.Point(12, 185);
+            this.availableBox.Name = "availableBox";
+            this.availableBox.Size = new System.Drawing.Size(457, 149);
+            this.availableBox.TabIndex = 12;
+            this.availableBox.TabStop = false;
+            this.availableBox.Text = "Cards available in Main Deck";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(12, 24);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(425, 109);
+            this.outputLabel.TabIndex = 6;
+            this.outputLabel.Text = "--";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(481, 346);
+            this.Controls.Add(this.availableBox);
+            this.Controls.Add(this.playerLabel);
+            this.Controls.Add(this.dealerLabel);
+            this.Controls.Add(this.collectButton);
+            this.Controls.Add(this.dealButton);
+            this.Controls.Add(this.shuffleButton);
+            this.Controls.Add(this.playerCardsLabel);
+            this.Controls.Add(this.dealerCardsLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Deck of Cards";
+            this.availableBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button shuffleButton;
+        private System.Windows.Forms.Button dealButton;
+        private System.Windows.Forms.Label dealerCardsLabel;
+        private System.Windows.Forms.Label playerCardsLabel;
+        private System.Windows.Forms.Button collectButton;
+        private System.Windows.Forms.Label dealerLabel;
+        private System.Windows.Forms.Label playerLabel;
+        private System.Windows.Forms.GroupBox availableBox;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ CardDeck/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardDeck
+{
+    public partial class Form1 : Form
+    {
+        // deck of cards
+        List<string> deck = new List<string>();
+        List<string> dealerCards = new List<string>();
+        List<string> playerCards = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //fill deck with standard 52 cards
+            //H - Hearts, D - Diamonds, C - Clubs, S - Spades
+
+            deck.AddRange(new string[] { "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH" });
+            deck.AddRange(new string[] { "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD" });
+            deck.AddRange(new string[] { "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC" });
+            deck.AddRange(new string[] { "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS" });
+
+            ShowDeck();
+
+            dealButton.Enabled = false;
+            collectButton.Enabled = false;
+          dealButton.BackColor = Color.White;
+            collectButton.BackColor = Color.White;
+         
+        }
+
+        public void ShowDeck()
+        {
+            outputLabel.Text = "";
+            for (int i = 0; i < deck.Count; i++)
+            {
+                outputLabel.Text += $"{deck[i].ToString()} ";
+            }
+        }
+
+        private void shuffleButton_Click(object sender, EventArgs e)
+        {
+            List<string> deckTemp = new List<string>();
+            Random randGen = new Random();
+
+            while (deck.Count > 0)
+            {
+                int index = randGen.Next(0, deck.Count);
+                deckTemp.Add(deck[index]);
+                deck.RemoveAt(index);
+            }
+
+            deck = deckTemp;
+
+            ShowDeck();
+            dealButton.Enabled = true;
+          shuffleButton.Enabled = false;
+            if (dealButton.Enabled == true)
+            {
+                dealButton.BackColor = Color.GreenYellow;
+            }
+            if (shuffleButton.Enabled == false)
+            {
+                shuffleButton.BackColor = Color.White;
+            }
+        }
+
+
+        private void dealButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                dealerCards.Add(deck[0]);
+                deck.RemoveAt(0);
+                playerCards.Add(deck[0]);
+                deck.RemoveAt(0);
+            }
+
+            dealerCardsLabel.Text = "";
+            playerCardsLabel.Text = "";
+
+            for (int i = 0; i < dealerCards.Count; i++)
+            {
+                dealerCardsLabel.Text += $"{dealerCards[i].ToString()} ";
+            }
+            for (int i = 0; i < playerCards.Count; i++)
+            {
+                playerCardsLabel.Text += $"{playerCards[i].ToString()} ";
+            }
+
+            ShowDeck();
+            collectButton.Enabled = true;
+            dealButton.Enabled = false;
+            if (dealButton.Enabled == false)
+            {
+                dealButton.BackColor = Color.White;
+            }
+            if (collectButton.Enabled == true)
+            {
+                collectButton.BackColor = Color.GreenYellow;
+            }
+        }
+
+        private void collectButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                deck.Add(dealerCards[0]);
+                dealerCards.RemoveAt(0);
+                deck.Add(playerCards[0]);
+                playerCards.RemoveAt(0);
+            }
+
+            dealerCardsLabel.Text = "";
+            playerCardsLabel.Text = "";
+
+            ShowDeck();
+
+            collectButton.Enabled = false;
+            shuffleButton.Enabled = true;
+            if (collectButton.Enabled == false)
+            {
+                collectButton.BackColor = Color.White;
+            }
+            if (shuffleButton.Enabled == true)
+            {
+                shuffleButton.BackColor = Color.GreenYellow;
+            }
+        }
+
+        private void dealerCardsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
